9da53b8e2e5cff1c89392fc5d589725bbd5c5ea9
tamerkat <mahad10taha@gmail.com>
2024-01-02 15:53:56 +0200

    py

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -13,12 +13,12 @@ int check_cycle(listint_t *list)
 {
 	listint_t *slow = list, *fast = list;
 
-	while (fast)
+	while (fast && fast->next)
 	{
-	slow = slow->next;
-	fast = fast->next->next;
-	if (slow == fast)
-	return (1);
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
 	}
 	return (0);
 }
