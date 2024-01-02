cca514d16ac69831a1b6a65a00431378858f44e9
tamerkat <mahad10taha@gmail.com>
2024-01-02 22:56:00 +0200

    py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    print('{:c}'.format(i), end='')
-    if i == "q" and i == "e":
-        continue
+    if i != ord("e") and i != ord("q"):
+        print('{:c}'.format(i), end='')
