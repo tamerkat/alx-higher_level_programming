710dbba9a1dcf8b371556864fcae9ae36a17ee83
tamerkat <mahad10taha@gmail.com>
2024-01-02 15:52:04 +0200

    py

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * check_cycle - Entry point
+ * @list: pointer
+ * Return: (0)
+*/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list, *fast = list;
+
+	while (fast)
+	{
+	slow = slow->next;
+	fast = fast->next->next;
+	if (slow == fast)
+	return (1);
+	}
+	return (0);
+}
