aa9231a759b94519a0c17690b32053488e7539db
tamerkat <mahad10taha@gmail.com>
2024-01-03 22:59:18 +0200

    py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("0{},".format(i), end=' ')
-    elif i > 99:
-        print("{},".format(i), end=' ')
-    else:
-        print("{}".format(i))
+    print("{:2d}".format(i), end='\n' if i == 99 else ', ')
