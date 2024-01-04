54bf2bdbb9f12487cdbd7beea68aa35fe1470b8f
tamerkat <mahad10taha@gmail.com>
2024-01-04 16:21:06 +0200

    py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,9 @@
-import sys
-ar = sys.argv[1:]
-x = 1
-print(f"{len(ar)} arguments:")
-for i in ar:
-    print(f"{x}: {i}")
-    x+=1
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    ar = sys.argv[1:]
+    x = 1
+    print(f"{len(ar)} arguments:")
+    for i in ar:
+        print(f"{x}: {i}")
+        x += 1
