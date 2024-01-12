fe30950a9da0e676188761d5b263e4f746234a73
tamerkat <mahad10taha@gmail.com>
2024-01-12 13:23:35 +0200

    py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     cop = ""
     for i in range(len(my_string)):
-        if my_string[i] != "c" and my_string[i] != "C":
+        if (my_string[i] != "c" and my_string[i] != "C"):
             cop += my_string[i]
     return cop
