21465b8caf61671d05263b860c056fe7b8066aa0
tamerkat <mahad10taha@gmail.com>
2024-01-12 13:17:19 +0200

    py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for i in matrix:
+        if len(i) == 0:
+            print()
+        for j in range(len(i)):
+            print("{:d}".format(i[j]), end="\n" if j is len(i) - 1 else " ")
