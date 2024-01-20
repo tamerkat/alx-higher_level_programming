660336b9873f346d2753ce185d5095b43ed5e83e
tamerkat <mahad10taha@gmail.com>
2024-01-20 14:11:21 +0200

    py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-output = ''.join(chr(i).lower() if i % 2 == 0 else chr(i).upper() for i in range(122, 64, -1))
-print("{}".format(output) end='')
+for i in range(25, -1, -1):
+    c = i + ord('A')
+    if i % 2 == 1:
+        c += 32
+    print("{:c}".format(c), end='')
