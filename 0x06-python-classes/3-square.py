78244e899786f79b92a68add30f7462367b00625
tamerkat <mahad10taha@gmail.com>
2024-01-28 14:30:33 +0200

    py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ square module """
+
+
 class Square:
     """ define square function """
     def __init__(self, size=0):
@@ -9,5 +11,6 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
-            return self.__size ** 2
+        return self.__size ** 2
