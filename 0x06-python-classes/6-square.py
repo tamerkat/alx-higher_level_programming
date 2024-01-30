f1724ff96f49cf0df7aa2b9d4abec9b326a5e082
tamerkat <mahad10taha@gmail.com>
2024-01-30 12:11:00 +0200

    py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""class Module"""
+
+
+class Square:
+    """define a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple and len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.size ** 2
+
+    def my_print(self):
+        for i in range(self.size):
+            print("#" * self.size)
+        if not self.size:
+            print()
