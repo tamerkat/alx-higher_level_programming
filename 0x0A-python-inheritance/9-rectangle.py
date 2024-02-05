5abaf6812394fd98a407e618c301b9dc20a9079b
tamerkat <mahad10taha@gmail.com>
2024-02-05 20:25:56 +0200

    py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
 
-    def are(self):
+    def area(self):
         return self.__width * self.__height
 
     def __str__(self):
