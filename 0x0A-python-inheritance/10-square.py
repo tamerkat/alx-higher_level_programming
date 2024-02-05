fb9e6616c7a5890fe92f84fab2ad3724bf70be76
tamerkat <mahad10taha@gmail.com>
2024-02-05 20:30:32 +0200

    py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""define class"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """"define class"""
+    def __init__(self, size):
+        size.integer_validator("size", size)
+        self.__size = size
+        
+    def area(self):
+        return self.__size ** 2
