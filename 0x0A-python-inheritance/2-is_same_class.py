e21caf05c8abb2ff659bc05e34533b9c7b0d735a
tamerkat <mahad10taha@gmail.com>
2024-02-05 18:40:18 +0200

    py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define class"""
+
+
+def is_same_class(obj, a_class):
+    """define function"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
