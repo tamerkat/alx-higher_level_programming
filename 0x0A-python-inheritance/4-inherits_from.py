d65c0b18aac8efdb90cb7aeb10ee555e1766928b
tamerkat <mahad10taha@gmail.com>
2024-02-05 18:48:53 +0200

    py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """define function"""
-    return isinstance(obj, a_class) and type(obj) == a_class
+    return isinstance(obj, a_class) and type(obj) != a_class
