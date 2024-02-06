0c8f240c3b259cfaf9ae3252e2a02f6bacd0fe76
tamerkat <mahad10taha@gmail.com>
2024-02-06 16:25:55 +0200

    py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+
+Reads and prints contents from file
+"""
+
+
+def write_file(filename="", text=""):
+    """Read and print text from file"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        print(f.write(text), end="")
