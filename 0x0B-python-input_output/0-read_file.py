e5d89e70fde650ed434e4910bb3ef836731e3adc
tamerkat <mahad10taha@gmail.com>
2024-02-06 16:12:23 +0200

    py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-    with open(filename, 'r', encoding='utf-8') as file:
+    with open(filename, encoding='utf-8') as file:
         for line in file:
             print(line.strip(), end='')
