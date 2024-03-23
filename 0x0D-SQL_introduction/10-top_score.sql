b54d74f2900fce03616f81b55c6b9fc653d99770
tamerkat <mahad10taha@gmail.com>
2024-03-23 11:36:42 +0200

    update

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- create table; shouldn't fail if already exists first_table second_table
 
-SELECT name, score FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
