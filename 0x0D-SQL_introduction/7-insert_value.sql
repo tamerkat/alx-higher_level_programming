1c7fb436b959b98084dc27d2f9528d7978055a6b
tamerkat <mahad10taha@gmail.com>
2024-03-23 11:21:13 +0200

    update

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- create table; shouldn't fail if already exists
 
-INSERT INTO `first_table` (`id`, `name`) VALUES(89, `Best School`)
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School')
