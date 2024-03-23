d785d9b08a8ae79ce4f9efeeaaaed868df776eb0
tamerkat <mahad10taha@gmail.com>
2024-03-23 13:20:12 +0200

    update

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,3 @@
 -- create table; shouldn't fail if already exists first_table second_table
 
-UPDATE second_table SET `score` = 10 WHERE `name` = Bob;
+UPDATE second_table SET `score` = 10 WHERE `name` = 'Bob';
