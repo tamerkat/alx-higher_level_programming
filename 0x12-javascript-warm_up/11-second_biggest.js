c1c67f96e891b4b67e48f15818cfa0a26f7827d1
tamerkat <mahad10taha@gmail.com>
2024-04-08 18:14:15 +0200

    js update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const nums = process.argv.slice(2);
+  const nums = process.argv;
   nums.sort((a, b) => b - a);
   console.log(nums[1]);
 }
