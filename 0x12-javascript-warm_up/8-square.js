54f9d7a634dfdff02abac0575a673ece016cb42f
tamerkat <mahad10taha@gmail.com>
2024-04-08 16:20:04 +0200

    js update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-const ar = process.argv[2];
+const ar = Math.floor(process.argv[2]);
 
 if (isNaN(ar)) {
   console.log('Missing size');
 }
 
 for (let i = 0; i < ar; i++) {
-  for (let j = 0; j < i; j++) {
-    console.log('x');
-  }
+  console.log('x' * ar);
 }
