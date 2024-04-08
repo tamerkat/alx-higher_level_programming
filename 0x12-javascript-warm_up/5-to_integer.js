44c5a2cee9f366896fa935921ff0014cba9efd7f
tamerkat <mahad10taha@gmail.com>
2024-04-08 15:49:53 +0200

    js update

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,9 @@
 
 const val = process.argv[2];
 
-if (typeof val === 'string') {
+if (!val) {
+  console.log('Not a number');
+} else if (typeof val === 'string') {
   console.log(`My number: ${parseInt(val)}`);
 } else {
   console.log(`My number: ${val}`);
