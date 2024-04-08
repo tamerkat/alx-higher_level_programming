f5564cf32b341b7447671b01aca0336f8a0f7d65
tamerkat <mahad10taha@gmail.com>
2024-04-08 12:20:37 +0200

    js update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
+argv.forEach((val, index) => {
+    console.log(`${index}: ${val}`);
+});
 if (argv.length = 1){
     console.log("No argument")
+}else if (argv.length = 2){
+    console.log("Argument found")
 }else{
     console.log("Arguments found")
 }
