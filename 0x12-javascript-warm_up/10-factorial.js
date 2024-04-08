9283dfe97d4c7dbc4b6c2c659a75091cce0255bc
tamerkat <mahad10taha@gmail.com>
2024-04-08 17:38:42 +0200

    js update

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@ function factorial (x) {
   if (isNaN(x) || x === 0) {
     return 1;
   }
-  x = x * factorial(x - 1);
+  return x * factorial(x - 1);
 }
 const num = +process.argv[2];
 const result = factorial(num);
