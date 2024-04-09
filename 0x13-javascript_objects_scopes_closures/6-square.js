f7fa420ac28f22417c78336f42e06cf51683cf7f
tamerkat <mahad10taha@gmail.com>
2024-04-09 15:21:02 +0200

    js update

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const DataSquare = require('./5-square.js');
+
+module.exports = class Square extends DataSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('C'.repeat(this.width));
+      }
+    }
+  }
+};
