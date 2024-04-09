2cae19c70dba1e21806df1641a048b4644bd77bc
tamerkat <mahad10taha@gmail.com>
2024-04-10 01:09:41 +0200

    js update

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 
-const doubleList = list.map((x, i) => { x * i});
+const newList = list.map((x, index) => x * index);
 
 console.log(list);
-console.log(doubleList);
+console.log(newList);
