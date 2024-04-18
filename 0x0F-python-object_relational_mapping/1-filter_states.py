6424a290b44f7906fcebb494a77accd57528a24c
tamerkat <mahad10taha@gmail.com>
2024-04-18 20:53:32 +0200

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                          db=argv[3])
 
     curser = db.cursor()
-    curser.execute("SELECT * FROM stats ORDER BY id ASC")
+    curser.execute("SELECT * FROM stats WHERE name LIKE N% ORDER BY id ASC")
     rows = curser.fetchall()
 
     for row in rows:
