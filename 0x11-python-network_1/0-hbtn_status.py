2c9d3e12bf179f5f1ce33b4c3355866aeb68f922
tamerkat <mahad10taha@gmail.com>
2024-04-30 16:35:15 +0300

    network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,3 +1,9 @@
 import urllib.request
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as url:
-    read = url.read()
+
+if __name__ == '__main__':
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as url:
+        read = url.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(url)))
+        print("\t- content: {}".format(url))
+        print("\t- utf8 content: {}".format(url.decode('utf-8')))
