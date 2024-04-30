aa3491865f6579771c023610939e84e43b598050
tamerkat <mahad10taha@gmail.com>
2024-04-30 15:54:32 +0300

    network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+    find the peak of list
+"""
+
+
 def find_peak(list_of_integers):
+    """
+        find the peak function
+    """
+
+    if len(list_of_integers) == 0:
+        return None
     list_copy = list_of_integers.copy()
     list_copy.sort()
     peak = list_copy[0]
