1fbeaef120fc0ea8d4bbbd79a1377bd1299d3a28
tamerkat <mahad10taha@gmail.com>
2024-05-21 15:56:47 +0300

    api task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const path = process.argv[1];
-const contant = process.argv[2];
+const path = process.argv[2];
+const contant = process.argv[3];
 
-fs.writeFile(path, contant + "\n", 'utf-8', (err) => {
+fs.writeFile(path, contant + '\n', 'utf-8', (err) => {
   if (err) {
     console.error(err);
   }
