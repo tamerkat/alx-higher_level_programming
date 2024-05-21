de22e60ada4036a7f0e0706ac5db0ef36c649882
tamerkat <mahad10taha@gmail.com>
2024-05-21 16:02:10 +0300

    api task 1

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const request = require('request');
 const url = process.argv[2];
+const request = require('request');
 
-request(url, {method: 'GET'}, (err, {statusCode}) => {
+request(url, { method: 'GET' }, (err, { statusCode }) => {
   if (err) return console.log(err);
   console.log(`code: ${statusCode}`);
 });
