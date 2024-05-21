0aaaa11dfcf579c5df6d095a573d5deaba2c93f2
tamerkat <mahad10taha@gmail.com>
2024-05-21 15:18:21 +0300

    api task 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,8 +4,9 @@ const fs = require('fs');
 const path = process.argv[2];
 
 fs.readFile(path, 'utf-8', (err, data) => {
-    if (err)
-        console.error(err);
-    else 
-        console.log(data);
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
